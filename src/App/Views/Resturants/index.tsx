ff968f6deeea870bdc35498900c1b397342d8443
Erik Wahlström <erik.wahlstrom@grebban.com>
2025-12-02 01:12:49 +0100

    fix: separaate resturant lkisting

--- src/App/Views/Resturants/index.tsx
+++ src/App/Views/Resturants/index.tsx
@@ -1,10 +1,10 @@
 import { useMemo } from "react";
-import { Text } from "App/components/Text";
 import { Logo } from "assets/media/Icons/Logo";
 import { useAllRestaurants } from "../../../hooks/useRestaurantData";
 import { useFilterParams } from "../../../hooks/useFilterParams";
 import { filterRestaurants } from "../../../utils/filterRestaurants";
 import Filters from "App/components/Filter/Filters";
+import { ResturantsListing } from "App/components/ResturantsListing";
 
 export const Resturants = () => {
   const { restaurants, isLoading, error } = useAllRestaurants();
@@ -54,43 +54,7 @@ export const Resturants = () => {
         />
       </div>
 
-      <div className="mt-6">
-        <Text
-          as="h2"
-          typography="Caps-Regular/12"
-          className="text-base-black-40 mb-4"
-        >
-          RESTAURANTS ({filteredRestaurants.length})
-        </Text>
-
-        {filteredRestaurants.length === 0 ? (
-          <Text typography="Regular/14" className="text-base-black-40">
-            No restaurants match your filters.
-          </Text>
-        ) : (
-          <div className="flex flex-col gap-4">
-            {filteredRestaurants.map((restaurant) => (
-              <div
-                key={restaurant.id}
-                className="p-4 border border-base-gray rounded-lg"
-              >
-                <Text
-                  as="h3"
-                  typography="Bold/16"
-                  className="text-base-black mb-2"
-                >
-                  {restaurant.name}
-                </Text>
-                {typeof restaurant.delivery_time_minutes === "number" && (
-                  <Text typography="Regular/14" className="text-base-black-40">
-                    {restaurant.delivery_time_minutes} min delivery
-                  </Text>
-                )}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      <ResturantsListing filteredRestaurants={filteredRestaurants} />
     </div>
   );
 };
