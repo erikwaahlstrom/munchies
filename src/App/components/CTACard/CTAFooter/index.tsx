90d66239aab06abd0efa2523beab3615d6085a6d
Erik Wahlström <erik.wahlstrom@grebban.com>
2025-12-03 09:05:30 +0100

    fix: update font

--- src/App/components/CTACard/CTAFooter/index.tsx
+++ src/App/components/CTACard/CTAFooter/index.tsx
@@ -4,7 +4,7 @@ import { Text } from "App/components/Text";
 export const CTAFooter = ({ name }: { name: string }) => {
   return (
     <div className="flex gap-2 justify-between items-end">
-      <Text as="h3" typography="Bold/16" className="text-base-black">
+      <Text as="h3" typography="Regular/24" className="text-base-black">
         {name}
       </Text>
 
